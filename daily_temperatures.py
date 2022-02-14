86a5bfc13a3fee714281d721e30a3d9e6ddaf5af
James Wong <jwong_li@hotmail.com>
2022-02-14 09:57:38 -0800

    Daily Temperature: use O(1) space complexity

--- daily_temperatures.py
+++ daily_temperatures.py
@@ -1,23 +1,39 @@
 
 
 def monotonic_stack(arr):
-    n = len(arr)
-    answer = [0 for _ in range(n)]
+    answer = [0 for _ in range(len(arr))]
     stack = []  # monotonic
 
-    for i in range(n):
-        j = len(stack) - 1
-        while j >= 0:
+    for i in range(len(arr)):
+        for j in range(len(stack) - 1, -1, -1):
             index, temp = stack[j]
             if arr[i] > temp:
                 answer[index] = i - index
                 stack.pop(j)
-            j -= 1
 
         stack.append((i, arr[i]))
     return answer
 
 
+def run(arr):
+    answer = [0 for _ in range(len(arr))]
+    hottest = arr[-1]
+
+    for i in range(len(arr) - 1, -1, -1):
+        curr_temp = arr[i]
+        if curr_temp >= hottest:
+            hottest = curr_temp
+        else:
+            j = 1
+            while arr[i + j] <= curr_temp:
+                j += answer[i + j]
+            answer[i] = j
+    return answer
+
+
+
+
 if __name__ == "__main__":
     temps = [72, 73, 78, 75, 74, 80, 77]
     print(monotonic_stack(temps))
+    print(run(temps))
