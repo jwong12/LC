147d8eca9ca5b19a6d28a3c2e939912cf5da0c3b
James Wong <jwong_li@hotmail.com>
2022-03-15 09:59:32 -0700

    add Decode String 2 Stacks solution

--- decode_string.py
+++ decode_string.py
@@ -1,6 +1,6 @@
 
 
-def decode_string(s: str) -> str:
+def decode_string(s):
     stack = []
 
     for c in s:
@@ -37,6 +37,43 @@ def decode_string(s: str) -> str:
     return res
 
 
+def decode_string_2stacks(s):
+    count_stack = []
+    string_stack = []
+    num_str = decoded_str = ""
+    i = 0
+
+    while i < len(s):
+        if s[i].isdigit():
+            while s[i].isnumeric():
+                num_str += s[i]
+                i += 1
+            continue
+
+        elif s[i] == '[':
+            count_stack.append(int(num_str))
+            string_stack.append(decoded_str)
+            num_str = decoded_str = ""
+
+        elif s[i] == ']':
+            if count_stack:
+                k = count_stack.pop()
+                temp_str = decoded_str
+                for _ in range(k-1):
+                    decoded_str += temp_str
+
+            if string_stack:
+                last_str = string_stack.pop()
+                decoded_str = last_str + decoded_str
+
+        else:
+            decoded_str += s[i]
+
+        i += 1
+
+    return decoded_str
+
+
 if __name__ == '__main__':
-    print(decode_string('3[a2[cd]]'))
+    print(decode_string_2stacks('3[a2[cd]]4[b]'))
 
