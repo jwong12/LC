5a768afa3dc841e1508549f01dfc9b74838942ad
James Wong <jwong_li@hotmail.com>
2022-04-08 10:48:33 -0700

    add Number of Islands BFS solution

--- number_of_islands.py
+++ number_of_islands.py
@@ -22,6 +22,37 @@ def num_islands(arr) -> int:
     return island_count
 
 
+def num_islands_bfs(arr) -> int:
+    nr = len(grid)
+    nc = len(grid[0])
+    island_count = 0
+    queue = []
+
+    for i in range(nr):
+        for j in range(nc):
+            if arr[i][j] == "1":
+                island_count += 1
+                queue.append((i, j))
+                arr[i][j] = "0"
+
+                while queue:
+                    row, col = queue.pop()
+                    if row > 0 and arr[row-1][col] == "1":
+                        queue.append((row-1, col))
+                        arr[row-1][col] = "0"
+                    if row < nr - 1 and arr[row+1][col] == "1":
+                        queue.append((row+1, col))
+                        arr[row+1][col] = "0"
+                    if col > 0 and arr[row][col-1] == "1":
+                        queue.append((row, col-1))
+                        arr[row][col-1] = "0"
+                    if col < nc - 1 and arr[row][col+1] == "1":
+                        queue.append((row, col+1))
+                        arr[row][col+1] = "0"
+
+    return island_count
+
+
 if __name__ == "__main__":
     grid = [
       ["1","1","0","0","0"],
@@ -29,4 +60,4 @@ if __name__ == "__main__":
       ["0","0","1","0","0"],
       ["0","0","0","1","1"]
     ]
-    print(num_islands(grid))
+    print(num_islands_bfs(grid))
